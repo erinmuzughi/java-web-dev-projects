905aac671c7e15fd3440fbfdc26ca859b91ac57b
erinmuzughi <erinmuzughi@gmail.com>
2023-09-14 08:52:55 -0500

    made it through testing inheritance

--- inheritance/testing-inheritance/exercises/src/main/java/Main.java
+++ inheritance/testing-inheritance/exercises/src/main/java/Main.java
@@ -14,5 +14,17 @@ public class Main {
         System.out.println(cheshireCat.noise()); // prints "Hello, my name is Cheshire!"
 
 //        Cat salem = new Cat(8);
+
+        Cat suki = new HouseCat("Suki", 8);
+
+        // Calls HouseCat's noise() method
+        System.out.println(suki.noise()); // Hello, my name is Suki!
+//        // Results in a compiler error, since Cat doesn't have such a method
+//        suki.isSatisfied();
+
+        // As long as suki really is a HouseCat, this works
+        System.out.println(((HouseCat) suki).isSatisfied());//prints true The danger here is that if suki is in fact
+        // not a HouseCat (it was declared only as a Cat, after all), then we’ll experience a runtime exception. So
+        // you should only cast an object to another type when you are very sure that it’s safe to do so.
     }
 }
---
+++ inheritance/testing-inheritance/exercises/src/test/java/HouseCatTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class HouseCatTest {
+    @Test
+    public void inheritsSuperInFirstConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+        assertEquals(7, keyboardCat.getWeight(), .001);
+    }
+
+    @Test
+    public void inheritsDefaultCatInSecondConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat");
+        assertEquals(13, keyboardCat.getWeight());
+    }
+
+    @Test
+    public void isNotInitiallyTired() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat");
+        assertFalse(keyboardCat.isHungry());
+        assertFalse(keyboardCat.isTired());
+        keyboardCat.eat();
+    }
+}
