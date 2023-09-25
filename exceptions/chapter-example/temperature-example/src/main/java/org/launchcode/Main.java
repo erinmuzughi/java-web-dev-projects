5d1a945db0a5f350a25b6e2bc450e66228a818a6
erinmuzughi <erinmuzughi@gmail.com>
2023-09-25 09:00:37 -0500

    chapter example and worked on exercise

--- exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/Main.java
+++ exceptions/chapter-example/temperature-example/src/main/java/org/launchcode/Main.java
@@ -2,6 +2,7 @@ package org.launchcode;
 
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
 
--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -4,7 +4,13 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        // Test out your Divide() function!
+        int x = 5;
+        int y = 5;
+        Divide (x, y);
+
+        int a = 5;
+        int b = 0;
+        Divide (a, b);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +19,34 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        CheckFileExtension(String.valueOf(studentFiles));
     }
 
     public static void Divide(int x, int y)
     {
-        // Write code here!
+        int grade;
+        try
+        {
+// performing division and storing th result
+            grade = x / y;
+            System.out.println("Division process has been done successfully.");
+            System.out.println("Result came after division is: " + grade);
+        }
+// handling the exception in the catch block
+        catch(java.lang.ArithmeticException ex)
+        {
+            System.out.println("Should avoid dividing by 0 " + ex);
+        }
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        int numPoints;
+        if (fileName.endsWith(".java") ) {
+            return numPoints = 1;
+        } else if (fileName.isEmpty() || fileName == null) {
+            return numPoints = -1;
+        }
+        return numPoints = 0;
     }
 }
